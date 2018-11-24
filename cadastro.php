8a3cb07a26e16ef70a50300e9f31e5bd8031e6cc
Hugo Almeida <almeida245@hotmail.com>
2018-11-24 18:43:13 -0200

    Update cadastro.php

--- cadastro.php
+++ cadastro.php
@@ -68,12 +68,8 @@ $senha = isset($candidato['senha']) ? $candidato['senha'] : '';
 			  <span class="step"></span>
 			  <span class="step"></span>
 			</div>
-
-		</form>
-
-		
-	</div>
-	
+		</form>		
+	</div>	
 
 	<script>
 		var currentTab = 0;
@@ -157,36 +153,15 @@ $senha = isset($candidato['senha']) ? $candidato['senha'] : '';
 				}
 			}
 
-
-		  // This function deals with validation of the form fields
-		  /*var x, y, i, valid = true;
-		  x = document.getElementsByClassName("tab");
-		  y = x[currentTab].getElementsByTagName("input");
-		  // A loop that checks every input field in the current tab:
-		  for (i = 0; i < y.length; i++) {
-		    // If a field is empty...
-		    if (y[i].value == "") {
-		      // add an "invalid" class to the field:
-		      y[i].className += " invalid";
-		      // and set the current valid status to false
-		      valid = false;
-		    }
-		  }
-		  // If the valid status is true, mark the step as finished and valid:
-		  if (valid) {
-		    document.getElementsByClassName("step")[currentTab].className += " finish";
-		  }*/
-		  return valid; // return the valid status
+			return valid;
 		}
 
 		function fixStepIndicator(n) {
-		  // This function removes the "active" class of all steps...
-		  var i, x = document.getElementsByClassName("step");
-		  for (i = 0; i < x.length; i++) {
-		    x[i].className = x[i].className.replace(" active", "");
-		  }
-		  //... and adds the "active" class on the current step:
-		  x[n].className += " active";
+		    var i, x = document.getElementsByClassName("step");
+		    for (i = 0; i < x.length; i++) {
+		        x[i].className = x[i].className.replace(" active", "");
+		    }
+		    x[n].className += " active";
 		}
 
 		function salvarCandidato(tipo, id){
